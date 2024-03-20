19cbe81e79f30e7bde5107afedd5186f2be80b2d
Ricardo Barbosa Baldrez <35501635+RicardoBaldrez@users.noreply.github.com>
2024-03-20 20:51:38 +0000

    Phrase modified

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Nathalia, eu te amo e se você me ama da uma risadinha!</h1>;
+  return <h1>Nathalia, eu te amo e se você me ama da uma risadinha! 😘</h1>;
 }
